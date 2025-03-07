98a7343a75bc98d3fca02f928b0af6692667dd55
Bogdan Bancila <bogdan.bancila@lseg.com>
2025-03-07 22:39:44 +0200

    fix

--- terraform/ec2-server.tf
+++ terraform/ec2-server.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "server" {
   
-	  count = "${var.InstanceCount}"
-	  ami = "${var.Ami_id}"
-	  instance_type = "${var.EC2_instance_type}"
+	count = "${var.InstanceCount}"
+	ami = "${var.Ami_id}"
+	instance_type = "${var.EC2_instance_type}"
   	security_groups = [aws_security_group.ec2_sg.id]
 
 }
